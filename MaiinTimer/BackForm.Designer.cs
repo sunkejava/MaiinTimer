1d3a4d51b1d80615866a161d520b74be6fcadd73
sunkejava <declineaberdeen@foxmail.com>
2019-02-12 21:44:40 +0800

    窗体动画修改

--- MaiinTimer/BackForm.Designer.cs
+++ MaiinTimer/BackForm.Designer.cs
@@ -663,6 +663,7 @@
             // 
             // BackForm
             // 
+            this.AnimationType = LayeredSkin.Forms.AnimationTypes.ThreeDTurn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
