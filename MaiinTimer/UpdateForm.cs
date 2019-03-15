e2750a688cf23ec0f1adc586b0d58255fdc0166e
sunkejava <declineaberdeen@foxmail.com>
2019-03-15 23:56:33 +0800

    添加更新窗体

---
+++ MaiinTimer/UpdateForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LayeredSkin.Forms;
+namespace BridImage
+{
+    public partial class UpdateForm : LayeredForm
+    {
+        public UpdateForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
